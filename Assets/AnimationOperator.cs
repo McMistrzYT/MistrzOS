936f7904fd4831763f5b0b75057b872d992b46ae
McMistrzYT <filipos0ek@gmail.com>
2020-10-07 14:21:36 +0200

    Added project

---
+++ Assets/AnimationOperator.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationOperator : MonoBehaviour
+{
+    public Animator[] WelcomeSetupPage;
+    public Animator[] SaveSetupPage;
+    public Animator[] UserSetupPage;
+    // public Animator transitionToOS;
+    // public float transitionToOSTime;
+
+    public float transitionTime = 1f;
+
+    public int currentProcedureNumber;
+
+    void Update()
+    {
+        // if (Input.GetButtonDown("Jump"))
+        // {
+        //     currentProcedureNumber += 1;
+        //     LoadNextSetupProcedure(currentProcedureNumber);
+        // }
+    }
+
+    public void LoadNextSetupProcedure(int nextProcedureNumber)
+    {
+        StartCoroutine(LoadSetupProcedure(nextProcedureNumber));
+    }
+
+    IEnumerator LoadSetupProcedure(int procedureNumber)
+    {
+        if(procedureNumber == 1)
+        {
+            foreach (var item in WelcomeSetupPage)
+            {
+                item.SetTrigger("Continue");
+            }
+        }
+        else if(procedureNumber == 2)
+        {
+            foreach (var item in SaveSetupPage)
+            {
+                item.SetTrigger("Continue");
+            }
+        }
+        else if (procedureNumber == 3)
+        {
+            foreach (var item in UserSetupPage)
+            {
+                item.SetTrigger("Continue");
+            }
+        }
+
+        yield return new WaitForSeconds(transitionTime);
+
+        if (procedureNumber == 1)
+        {
+            foreach (var item in SaveSetupPage)
+            {
+                item.SetTrigger("Start");
+            }
+        }
+        else if (procedureNumber == 2)
+        {
+            foreach (var item in UserSetupPage)
+            {
+                item.SetTrigger("Start");
+            }
+        }
+        else if (procedureNumber == 3)
+        {
+            foreach (var item in UserSetupPage){
+                item.SetTrigger("Continue");
+            }
+            yield return new WaitForSeconds(transitionTime + 0.5f);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
---
+++ Assets/DataPathString.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class DataPathString : MonoBehaviour
+{
+    public TextMeshProUGUI persistentDataPathText;
+    [Tooltip("Tags: {DATAPATH} = Persistent Data Path")][TextArea(2,5)] public string stringToShow = "Your save data will be stored in {DATAPATH}.";
+
+    private void Start()
+    {
+        string editedStringToShow = stringToShow.Replace("{DATAPATH}", Application.persistentDataPath);
+        persistentDataPathText.text = editedStringToShow;
+    }
+}
